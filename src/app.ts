ffa6433b95b5b3da3c4912def7cc39ca4e029f3a
jaroslawmuzelak <jaroslaw.muzelak@gmail.com>
2020-05-31 21:32:23 +0200

    Added dockerfile and docker-compose

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 const app = express();
-
+const port = 8080;
 
 let waregouseRouter = require ('./api/routes/warehouseRoutes');
 let homeRouter = require ('./api/routes/homeRoutes');
@@ -9,4 +9,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/warehouse', waregouseRouter)
 app.use('/', homeRouter);
-app.listen(3000, () =>console.log(`App listening at http://localhost:3000`))
+app.listen(port, () =>console.log(`App listening at http://localhost:${port}`))
+
